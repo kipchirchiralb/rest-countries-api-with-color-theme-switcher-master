e2030c7495100a0fd7edf3ac8f3716da45849cdc
albert Kipchirchir <albertkipchirchir02@gmail.com>
2024-05-13 16:16:36 +0300

    render all countries

---
+++ country.js
@@ -0,0 +1,23 @@
+fetch("./data.json")
+.then(res=>res.json())
+.then(allcountries=>{
+    renderCountries(allcountries)
+
+})
+.catch(err=>{
+    console.log(err);
+})
+
+const allcountriesDiv = document.getElementById("all-countries")
+function renderCountries(countries){
+    allcountriesDiv.innerHTML = ""
+    countries.forEach(country=>{
+        let countryDiv = document.createElement("div")
+        countryDiv.innerHTML = `
+        <img src= ${country.flags.png} alt=${country.name + "flag"}>
+        <h2>Name:${country.name} </h2>
+        <p> Population: ${country.population} </p>`;
+             
+        allcountriesDiv.appendChild(countryDiv);
+    });
+}
