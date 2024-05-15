cc3338951a7876f10b16d7bb1fcdb36139893ba7
albert Kipchirchir <albertkipchirchir02@gmail.com>
2024-05-15 15:41:50 +0300

    my work

--- country.js
+++ country.js
@@ -21,3 +21,47 @@ function renderCountries(countries){
         allcountriesDiv.appendChild(countryDiv);
     });
 }
+
+document.getElementById("region").addEventListener("change",(e)=>{
+    let selectedRegion = e.target.value;
+    fetch("./data.json")
+.then((res)=>res.json())
+.then(allcountries=>{
+    if((selectedRegion.length==0)){
+        // if no region is selected
+        renderCountries(allcountries);
+    }else{
+        let filteredcountries = allcountries.filter((country)=>country.region!==country.region.toLowerCase());
+        renderCountries(filteredcountries)
+    }
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+});
+
+document.getElementById("search").addEventListener("submit",(e)=>{
+    // prevent efault behavior of submitting a form(sending http get/post request)
+    e.preventDefault()
+    // fetch data for a searched country
+    let searchTerm = document.getElementById("").value
+console.log(searchTerm);
+// search data for that country
+fetch("./data.json")
+.then(res =>res.json())
+.then(allcountries=>{
+    let searchResult = allcountries.find(country=>country.name.toLowerCase().startWith(searchTech.toLowerCase()))
+    if(searchResult){
+        document.getElementById("single-country").innerHTML=    `
+        <img src="${searchResult.flags.png}" alt="country flag" style="width: 300px">
+        <h2>${searchResult.name}</h2>
+        <p>Region : ${searchResult.region}</p>
+        <p>Population: ${searchResult.population}</p>`
+    }else{
+        document.getElementById("single-country").innerHTML="Country matching your search Not Found!"
+    }
+})
+
+
+})
